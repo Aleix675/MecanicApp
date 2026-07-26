b42021abe769cf822789bc1ae6f42ae7c0ce6627
aleix675 <aleix675cc@gmail.com>
2026-07-26 19:38:38 +0200

    configurar variables de entorno

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+
 from fastapi import FastAPI
 from app.database.connection import engine
 from app.database import base, connection
@@ -15,6 +18,10 @@ from app.routers import usuaris,serveis,vehicles,cites,auth,admin,notes
 from sqlalchemy.orm import Session
 
 #INICIAR APP -->dins de backend -->[   python -m uvicorn app.main:app --reload    ]<--
+
+# Cargar las variables de entorno (.env)
+load_dotenv()
+
 app = FastAPI(
     title="MecànicApp API",
     description="API REST per a gestió de taller mecànic",
@@ -22,17 +29,26 @@ app = FastAPI(
     docs_url="/docs",      
     redoc_url="/redoc"     
 )
+
+# 1. Definir los orígenes locales por defecto (siempre se van a usar en desarrollo)
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+# 2. Leer la URL del frontend en producción desde el .env si existe
+frontend_prod_url = os.getenv("FRONTEND_URL")
+if frontend_prod_url:
+    origins.append(frontend_prod_url)
+
+# 3. Aplicar el middleware con la lista dinámica
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://mecanic-app-theta.vercel.app"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-#sql supabase URI postgresql://postgres:[YOUR-PASSWORD]@db.nknlpsdzvwcwemywynxu.supabase.co:5432/postgres
-
-#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:+?P?xZs7-xPfzZP@db.nknlpsdzvwcwemywynxu.supabase.co:5432/postgres";
 
 
 app.include_router(usuaris.router)
